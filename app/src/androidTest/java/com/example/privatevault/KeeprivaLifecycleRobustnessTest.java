454d3da7931867cc662c68426618d1fc3b029485
skSubin <kssubin@gmail.com>
2026-09-20 22:06:53 +0530

    fixed issues

--- app/src/androidTest/java/com/example/privatevault/KeeprivaLifecycleRobustnessTest.java
+++ app/src/androidTest/java/com/example/privatevault/KeeprivaLifecycleRobustnessTest.java
@@ -4,6 +4,7 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;

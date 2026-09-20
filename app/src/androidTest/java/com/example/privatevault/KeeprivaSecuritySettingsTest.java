d25e43993e3c50ffc75ee7796d7608a1bece7721
skSubin <kssubin@gmail.com>
2026-09-20 21:56:46 +0530

    fixed issues

--- app/src/androidTest/java/com/example/privatevault/KeeprivaSecuritySettingsTest.java
+++ app/src/androidTest/java/com/example/privatevault/KeeprivaSecuritySettingsTest.java
@@ -7,7 +7,8 @@ import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;`nimport static androidx.test.espresso.matcher.ViewMatchers.withHint;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
